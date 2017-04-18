c0d861cffa6b9e6be9e9e259249c8ad686bc888e
GhostClock <zhangyuanlaifen@gmail.com>
2017-04-18 23:47:40 +0800

    模拟知乎登录

---
+++ LoginzhihuDemo.py
@@ -0,0 +1,36 @@
+import requests
+url = "https://www.zhihu.com/#signin"
+
+z = requests.get(url)
+print z.status_code
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+}
+
+z1 = requests.get(url,headers=headers)
+print z1.status_code
+
+from lxml import etree
+
+sel = etree.HTML(z1.content)
+
+_xsrf = sel.xpath('//input[@name="_xsrf"]/@value')[0]
+print _xsrf
+
+# 模拟登录
+loginurl = 'https://www.zhihu.com/login/email'
+fromdata = {
+    '_xsrf':_xsrf,
+    'password':'yuanhappy131',
+    'email':'zhangyuanlaifen@163.com'
+}
+z2 = requests.post(url=loginurl,data=fromdata,headers=headers)
+
+print z2.status_code
+
+headers['Cookie'] = z2.headers['Set-Cookie']
+mylog = "https://www.zhihu.com/people/pa-chong-21/logs"
+z3 = requests.get(url=mylog,headers=headers)
+print z3.url
+print z3.text
