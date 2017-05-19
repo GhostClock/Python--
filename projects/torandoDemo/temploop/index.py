39454ee04608622c6d9afed2fc00b77172ede5ce
GhostClock <GhostClock@GhostClock.local>
2017-05-19 18:23:56 +0800

    增加tornado模板工程

--- projects/torandoDemo/temploop/index.py
+++ projects/torandoDemo/temploop/index.py
@@ -24,11 +24,12 @@ handlers = [
 ]
 
 template_path = os.path.join(os.path.dirname(__file__),"temploop") #模板路径
+static_path = os.path.join(os.path.dirname(__file__),"stitic") #这里增加设置了静态路径
 
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers,template_path)
+    app = tornado.web.Application(handlers,template_path,static_path,debug=True) # 这里也增加了static 这里的debug=true的目的是为了让torando不重启也能够体现已经修改的代码功能
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
